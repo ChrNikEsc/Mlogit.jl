55cf04ee049327c499d338d0c406511cbea55d64
Christian Eschenweck <chr.nik.esc@gmail.com>
2025-03-20 15:30:39 +0100

    including test for nlogit

--- src/Mlogit.jl
+++ src/Mlogit.jl
@@ -9,10 +9,12 @@ using StatsBase, StatsAPI, Optim, RegressionTables, DataStructures, Vcov, Distri
 using Optim, Distributions, LineSearches
 import ForwardDiff
 using FiniteDifferences
+using LinearAlgebra: diag, dot
 
 
 
 export mlogit
+export nests
 
 include("utils.jl")
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,11 @@ using Test
 
 using CSV, DataFrames
 
-const glm_datadir = joinpath(dirname(@__FILE__), "..", "data/")
+const mlogit_datadir = joinpath(dirname(@__FILE__), "..", "data/")
 
-electrictiy_long = CSV.read(glm_datadir * "electricity_long.csv", DataFrame)
+# mlogit
+electrictiy_long = CSV.read(mlogit_datadir * "electricity_long.csv", DataFrame)
 # subset!(electrictiy_long, :id => x -> x .∈ [1:100])
-
 df_mlogit = electrictiy_long
 transform!(groupby(df_mlogit, :chid), eachindex => :alt)
 transform!(df_mlogit, :id => (id -> 2 * id .- 5) => :x1)
@@ -15,6 +15,11 @@ transform!(df_mlogit, :x1 => (x1 -> 1 / 2 * x1) => :x2)
 transform!(df_mlogit, :id => (x -> x / sum(unique(df_mlogit.id))) => :weight)
 transform!(df_mlogit, :id => (x -> ifelse.(x .> 30, "a", "b")) => :cluster)
 
+# nlogit
+HCdata = CSV.read(mlogit_datadir * "HCdata.csv", DataFrame)
+rename!(HCdata, Symbol.(replace.(String.(names(HCdata)), "." => "_")))
+transform!(HCdata, :id => :chid)
+transform!(HCdata, :alt => ByRow(x -> ifelse(x ∈ ["gcc", "ecc", "erc", "hpc"], "cooling", "others")) => :nest)
 
 @testset "Mlogit.jl" begin
     # mlogit
@@ -25,4 +30,13 @@ transform!(df_mlogit, :id => (x -> ifelse.(x .> 30, "a", "b")) => :cluster)
     )
 
     @test sum(model_mlogit.coef) ≈ -10.368346014522867
+
+    # nlogit
+    model_nlogit = mlogit(
+        @formula(choice ~ ich + och + icca + occa + inc_room + inc_cooling + int_cooling + nests(nest)),
+        HCdata,
+        equal_lambdas=false
+    )
+
+    @test sum(model_nlogit.coef) ≈-7.895003155761888
 end
