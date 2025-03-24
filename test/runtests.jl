32f5af9d1361e4755249969a783a4e8fa694d321
Christian Eschenweck <chr.nik.esc@gmail.com>
2025-03-24 11:06:38 +0100

    adding tests for lclogit

--- test/runtests.jl
+++ test/runtests.jl
@@ -51,7 +51,10 @@ df_lclogit = CSV.read(mlogit_datadir * "statadata_lclogit2_classes7_seed10329.cs
 
     # lclogit
     model_lclogit_em = lclogit(@formula(choice ~ pf + cl + loc + wk + tod + seas + membership(x1)), df_lclogit, 4, method=:em)
+    @test round(sum(model_lclogit_em.coef_memb), digits=5) ≈ -1.06644
+    
     model_lclogit_grad = lclogit(@formula(choice ~ pf + cl + loc + wk + tod + seas + membership(x1)), df_lclogit, 4, start_mnl=model_lclogit_em.coef_mnl, start_memb=model_lclogit_em.coef_memb, method=:gradient)
+    @test round(sum(model_lclogit_grad.coef_memb), digits=5) ≈ -1.06492
 end
 
 
